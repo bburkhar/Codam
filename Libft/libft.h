77f98ec2f267ecbfedbaea871caa1ede58d3ab30
bburkhar <brunoburkhardtb@gmail.com>
2019-11-05 15:04:50 +0100

    update

---
+++ Libft/libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libft.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bburkhar <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/11/05 12:32:46 by bburkhar      #+#    #+#                 */
+/*   Updated: 2019/11/05 13:36:24 by bburkhar      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+	void		*ft_memchr(const void *s, int c, size_t n);
+#endif

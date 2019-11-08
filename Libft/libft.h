1d0c74e05a11c61ac83304eca1df0550ab9c23c8
Bruno <brunoburkhardtb@gmail.com>
2019-11-08 10:18:50 +0100

    update

--- Libft/libft.h
+++ Libft/libft.h
@@ -5,8 +5,8 @@
 /*                                                     +:+                    */
 /*   By: bburkhar <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2019/11/05 12:32:46 by bburkhar      #+#    #+#                 */
-/*   Updated: 2019/11/05 13:36:24 by bburkhar      ########   odam.nl         */
+/*   Created: 2019/11/05 12:32:46 by bburkhar       #+#    #+#                */
+/*   Updated: 2019/11/08 10:18:37 by bruno         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+	int			ft_atoi(const char *str);
+	void		ft_bzero(void *s, size_t n);
+	void		*ft_calloc(size_t count, size_t size);
+	int			ft_isalnum(int c);
+	int			ft_isalpha(int c);
 	void		*ft_memchr(const void *s, int c, size_t n);
 #endif

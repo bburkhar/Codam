a606a137d5c37c8e0ac32a33d2f9c9468be03c6d
bburkhar <brunoburkhardtb@gmail.com>
2019-11-13 18:32:30 +0100

    update

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,59 +6,91 @@
 /*   By: bburkhar <bburkhar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/08 18:13:06 by bburkhar       #+#    #+#                */
-/*   Updated: 2019/11/13 00:16:48 by bruno         ########   odam.nl         */
+/*   Updated: 2019/11/13 17:10:47 by bburkhar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int     ft_strlen(const char *str)
+{
+    int i; 
+
+    i = 0;
+    while (str[i] != '\0')
+        ++i;
+    return (i);
+}
+
 int     ft_count(const char *s, char c)
 {
     int i;
     int a;
-
+    int x;
+    
     i = 0;
     a = 0;
-    while (s[i] != '\0')
+    x = ft_strlen(s);
+    if (s[i] == c)
     {
-        if (s[i] == c)
+        while (s[i] == c)
+            ++i;
+    }
+    if (s[x] == c)
+    {
+        while (s[x] == c)
+            --x;
+    }
+    while (i != x)
+    {
+        if (s[i] == c && s[i - 1] != c)
             ++a;
         ++i;
     }
-    return (a + 1);
+    ++a;
+    return (a);
 }
 
-int     *ft_wordsizes(const char *s, char c)
+int     *ft_wordsizes(const char *s, char c, int j)
 {
     int i;
-    int j;
     int a;
     int *wordsizes;
     int last;
+    int x;
 
     i = 0;
     a = 0;
-    last = 0;
-    j = ft_count(s, c);
     wordsizes = malloc(sizeof(int) * j);
+    if (s[i] == c)
+    {
+        while (s[i] == c)
+            ++i;
+    }
+    last = i;
     while (s[i] != '\0')
     {
-        if (s[i] == c)
+        if (s[i] == c || s[i + 1] == '\0')
         {
+            while (s[i] == c)
+            {
+                ++i;
+                ++last;
+            }
+            if (s[i] != c && s[i + 1] == '\0')
+                last = last - 1;
             wordsizes[a] =  i - last;
             last = i;
             ++a;
         }
         ++i;
     }
-    if (s[i - 1] == c)
-        wordsizes[a] = 1;
     return (wordsizes);
 }
 
 char    **ft_split(const char *s, char c)
 {
-    char **new; 
+    char **new;
     int *wordsizes;
     unsigned int sizes;
     int i;
@@ -72,7 +104,7 @@ char    **ft_split(const char *s, char c)
     sizes = 0;
     x = 0;
     new = malloc(sizeof(char *) * i);
-    wordsizes = ft_wordsizes(s, c);
+    wordsizes = ft_wordsizes(s, c, i);
     while (a < i)
     {
         new[a] = malloc(sizeof(char)* wordsizes[sizes] + 1);
@@ -80,6 +112,8 @@ char    **ft_split(const char *s, char c)
         j = 0;
         while (copy > 0)
         {
+            while (s[x] == c)
+                ++x;
             new[a][j] = s[x];
             ++j;
             --copy;
@@ -94,24 +128,41 @@ char    **ft_split(const char *s, char c)
 
 int     main(void)
 {
-    char str[] = "HalloeHalloeHalloeHalloeeeeeHallooooeeee";
+    char str[] = "    Hallo Hallo Hallo Hallo e ";
     char **ptr;
     char a;
     int i;
     int c;
+    int *test;
 
-    a = 'e';
+    a = ' ';
+    test = ft_wordsizes(str, a, ft_count(str, a));
     i = 0;
     c = 0;
+
+    while (c < ft_count(str,a))
+    {
+        printf("%d", test[c]);
+        ++c;
+    }
+    printf("\n");
+    c = 0;
+    printf("%d\n", ft_count(str, a));
+    if (str[i] == c)
+    {
+        while (str[i] == c)
+            ++i;
+    }
     while (str[i] != '\0')
     {
-        if (str[i] == a)
+        if (str[i] == a && str[i - 1] != a)
             ++c;
         ++i;
     }
+
     i = 0;
     ptr = ft_split(str, a);
-    while (i <= c)
+    while (i < ft_count(str, a))
     {    
         printf("%s\n", ptr[i]);
         ++i;
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: bburkhar <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/05 12:32:46 by bburkhar       #+#    #+#                */
-/*   Updated: 2019/11/08 10:18:37 by bruno         ########   odam.nl         */
+/*   Updated: 2019/11/13 18:07:51 by bburkhar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-	int			ft_atoi(const char *str);
+	void		*ft_memset(void *b, int c, size_t len);
 	void		ft_bzero(void *s, size_t n);
-	void		*ft_calloc(size_t count, size_t size);
-	int			ft_isalnum(int c);
-	int			ft_isalpha(int c);
+	void 		*ft_memcpy(void *dst, const void *src, size_t n);
+	void		*ft_memccpy(void *restrict dst, const void *restrict src, int c, size_t n);
+	void		*ft_memmove(void *dst, const void *src, size_t len);
 	void		*ft_memchr(const void *s, int c, size_t n);
+	int 		ft_memcmp(const void *s1, const void *s2, size_t n);
+	int			ft_strlen(const char *str);
+	size_t		ft_strlcpy(char *dest, const char *src, size_t len);
+	size_t		ft_strlcat(char *dest, const char *src, size_t len);
+	char		*ft_strchr(const char *s, int c);
+	char		*ft_strrchr(const char *s, int c);
+	char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+	int			ft_strncmp(const char *s1, const char *s2, size_t n);
+	int			ft_atoi(const char *str);
+	int			ft_isalpha(int c);
+	int			ft_isdigit(int c);
+	int			ft_isalnum(int c);
+	int 		ft_isascii(int c);
+	int			ft_isprint(int c);
+	int			ft_toupper(int c);
+	int			ft_tolower(int c);
+	void		*ft_calloc(size_t count, size_t size);
+	char		*ft_strdup(const char *s1);
+
 #endif

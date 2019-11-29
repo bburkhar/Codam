d07fa0e90cda22c1d6a080a6de4603a4e197486f
bburkhar <brunoburkhardtb@gmail.com>
2019-11-29 18:22:09 +0100

    bugfixes

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,30 +6,30 @@
 /*   By: bburkhar <bburkhar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/31 13:43:41 by bburkhar       #+#    #+#                */
-/*   Updated: 2019/11/25 15:13:13 by bburkhar      ########   odam.nl         */
+/*   Updated: 2019/11/29 18:13:44 by bburkhar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-    unsigned char *str1;
-    const unsigned char *str2;
-    size_t i;
-    size_t j;
+	unsigned char			*str1;
+	const unsigned char		*str2;
+	size_t					i;
+	size_t					j;
 
-    str1 = (unsigned char *)dst;
-    str2 = (unsigned char *)src;
-    i = 0;
-    j = 0;
-    if (n == 0 || dst == src)
-        return (dst);
-    while (i < n)
-    {
-        ((char *)str1)[j] = ((char *)str2)[j];
-        j++;
-        i++;
-    }
-    return (str1);
+	str1 = (unsigned char *)dst;
+	str2 = (unsigned char *)src;
+	i = 0;
+	j = 0;
+	if (n == 0 || dst == src)
+		return (dst);
+	while (i < n)
+	{
+		((char *)str1)[j] = ((char *)str2)[j];
+		j++;
+		i++;
+	}
+	return (str1);
 }
--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,26 +6,25 @@
 /*   By: bburkhar <bburkhar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/31 11:36:28 by bburkhar       #+#    #+#                */
-/*   Updated: 2019/11/25 14:55:48 by bburkhar      ########   odam.nl         */
+/*   Updated: 2019/11/29 18:21:29 by bburkhar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-    unsigned char *str;
-    unsigned char a;
-    size_t i;
+	unsigned char		*str;
+	unsigned char		a;
+	size_t				i;
 
-    a = (unsigned char) c; 
-    str = (unsigned char *) b;
-    i = 0;
-    while (i < len)
-    {
-        ((char *) str)[i] = a;
-        ++i;
-    }
-        
-    return (str);
+	a = (unsigned char)c;
+	str = (unsigned char *)b;
+	i = 0;
+	while (i < len)
+	{
+		((char *)str)[i] = a;
+		++i;
+	}
+	return (str);
 }
